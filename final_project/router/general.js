29d6ad8dab4ebdd40baa83d9aafcd10203b80018
Arnavanand1508 <arnavanand0815@gmail.com>
2025-02-14 14:38:52 -0500

    commited changes

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,22 +10,35 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
+
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  try {
+    res.send(JSON.stringify(books, null, 2)); // Neatly format JSON output
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving book list" });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn; // Retrieve the ISBN from request parameters
   
+    if (books[isbn]) {
+      res.json(books[isbn]); // Return the book details in JSON format
+    } else {
+      res.status(404).json({ message: "Book not found" }); // Handle invalid ISBN
+    }
+});
+    
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.author === author))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
 // Get all books based on title
